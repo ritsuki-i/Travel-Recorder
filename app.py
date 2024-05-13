6626b2de815f636c5917a7e15a0bc8343f86a916
unknown <ritsuki-i>
2024-05-13 19:43:02 +0900

    ログイン機能

--- app.py
+++ app.py
@@ -1,11 +1,49 @@
-from flask import Flask, redirect, render_template, request
+from flask import Flask
+from flask import render_template , request
+from flask import render_template , request, jsonify
+from src.User import User
 
 app = Flask(__name__)
+User = User()
+
 
 @app.route('/', methods=['GET'])
 def login():
-    return render_template('login.html')
-    
+    if request.method == 'GET':
+        User.UserEmail =  None
+        User.UserID =  None
+        return render_template(
+        'login.html'
+    )
+    else:
+        return render_template(
+            'login.html'
+        )
+
+
+@app.route('/toMyMap', methods=['GET','POST'])
+def toMyMap():
+    if request.method == 'GET':
+        if User.UserEmail !=  None and User.UserID !=  None:
+            return render_template(
+                'mymap.html', UserEmail=User.UserEmail, UserID=User.UserID
+            )
+        else:
+            return render_template(
+            'login.html'
+            )
+    else:
+        UserData = request.json
+        print(type(UserData))
+        User.UserEmail = UserData['User']['email']
+        User.UserID = UserData['User']['uid']
+        return jsonify({'message': 'Data received by Python'})
+
+@app.route('/CreateAccount', methods=['GET','POST'])
+def CreateAccount():
+    return render_template(
+        'createAccount.html'
+    )
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(debug=True)
---
+++ static/js/Authentication.js
@@ -0,0 +1,151 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-app.js";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  sendEmailVerification,
+  sendPasswordResetEmail,
+} from "https://www.gstatic.com/firebasejs/10.10.0/firebase-auth.js";
+
+import firebaseConfig from "./loginapi.json" assert { type: "json" };
+
+
+const app = initializeApp(firebaseConfig);
+
+const provider_google = new GoogleAuthProvider();
+const auth = getAuth(app);
+auth.languageCode = "en";
+
+const googleLogin = document.getElementById("login-google");
+googleLogin?.addEventListener("click", function () {
+  signInWithPopup(auth, provider_google)
+    .then((result) => {
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      const user = result.user;
+      const dataToPython = { User: user, Token: token };
+
+      fetch("/toMyMap", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(dataToPython),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log("Data from Python:", data);
+          window.location.href = "/toMyMap";
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+});
+
+const LoginEmail = document.getElementById("LoginEmail");
+const LoginPass = document.getElementById("LoginPass");
+const emailpassLogin = document.getElementById("login-emailpass");
+emailpassLogin?.addEventListener("click", function () {
+  signInWithEmailAndPassword(auth, LoginEmail.value, LoginPass.value)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      const token = userCredential.accessToken;
+      const dataToPython = { User: user, Token: token };
+      const emailVerified = user.emailVerified;
+
+      if (emailVerified){
+        fetch("/toMyMap", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(dataToPython),
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            console.log("Data from Python:", data);
+            window.location.href = "/toMyMap";
+          })
+          .catch((error) => {
+            alert(error);
+            console.error("Error:", error);
+          });
+      } else {
+        alert("メールアドレスの認証が完了しておりません");
+      }
+    })
+    .catch((error) => {
+      alert("パスワードが正しくありません");
+      console.error("Error:", error);
+    });
+});
+
+const inputEmail = document.getElementById("inputEmail");
+const inputPass = document.getElementById("inputPass");
+const CreateAccount = document.getElementById("CreateAccount");
+CreateAccount?.addEventListener("click", function () {
+  if (!inputEmail.value || !inputPass.value) {
+    alert("空文字またはnullです");
+  } else {
+    createUserWithEmailAndPassword(auth, inputEmail.value, inputPass.value)
+      .then((userCredential) => {
+        sendEmailVerification(auth.currentUser)
+          .then(() => {
+            alert("メールアドレス確認用のメールを送信しました。承認してください。");
+          });
+      })
+      .catch((error) => {
+        if (
+          error.message ===
+          "Firebase: Password should be at least 6 characters (auth/weak-password)."
+        ) {
+          alert("パスワードは6文字以上で設定してください");
+        } else if (error.message === "Firebase: Error (auth/invalid-email).") {
+          alert("存在しないメールアドレスです");
+        } else if (
+          error.message === "Firebase: Error (auth/email-already-in-use)."
+        ) {
+          alert("そのメールアドレスはすでに登録されています。");
+        } else {
+          alert(error.message);
+          console.log(error.message);
+        }
+      });
+  }
+});
+
+
+const ChangePassword = document.getElementById("ChangePassword");
+ChangePassword?.addEventListener("click", function () {
+  if (!inputEmail.value) {
+    alert("メールアドレスが入力されていません");
+  } else {
+    sendPasswordResetEmail(auth, inputEmail.value)
+    .then(() => {
+      alert("パスワード変更のメールを送信しました。送信されたメールからパスワードを変更してください。");
+    })
+    .catch((error) => {
+      alert("そのメールアドレスは登録されておりません。");
+      console.error(error);
+    });
+  }
+});
+
+const Logout = document.getElementById("logout-btn");
+Logout?.addEventListener("click", function () {
+  signOut(auth)
+    .then(() => {
+      window.location.href = "/";
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+});
+
