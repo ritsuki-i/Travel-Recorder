da39d55fd25823759695880a5554ea12ba68275e
hamo <63552479+Hamo203@users.noreply.github.com>
2024-05-13 18:52:14 +0900

    template flask

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
-from flask import Flask, redirect, render_template, request
+from flask import Flask
+from flask import render_template , request
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/')
 def login():
-    return render_template('login.html')
-    
+    return render_template(
+        'login.html'
+    )
 
+@app.route('/mymap')
+def mymap():
+    return render_template(
+        'mymap.html'
+    )
+    
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(debug=True)
