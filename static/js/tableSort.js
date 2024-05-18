1dda4d87a16b9f5e7598cb887cf4f753ba5f94e4
YukinoSatou <yukino0513@gmail.com>
2024-05-18 17:11:05 +0900

    とりあえずコミット

--- static/js/tableSort.js
+++ static/js/tableSort.js
@@ -1,3 +1,4 @@
+
 document.addEventListener('DOMContentLoaded', () => {
   const getCellValue = (row, columnName) => {
     const cell = row.querySelector(`[data-column="${columnName}"]`) || row.children[{
@@ -5,15 +6,27 @@ document.addEventListener('DOMContentLoaded', () => {
       'description': 1,
       'date': 2,
     }[columnName]];
+
+    if (columnName === 'label') {
+      // columnNameが'label'の場合、ラベルの言語に応じてlocaleを選択
+      const labelValue = cell ? cell.textContent || cell.innerText : '';
+      return labelValue.trim().match(/[\u3040-\u309F\u30A0-\u30FF\u4E00-\u9FAF\u3400-\u4DBF]/) ? 'ja' : 'en';
+    }
+
+
     return cell ? cell.textContent || cell.innerText : '';
   };
+
 
   const compare = (a, b, columnName, order) => {
     const aValue = getCellValue(a, columnName);
     const bValue = getCellValue(b, columnName);
-    const compareResult = aValue.localeCompare(bValue, undefined, { numeric: true, sensitivity: 'base' });
+    const locale = columnName === 'label' ? 'ja' : 'en';
+    const compareResult = aValue.localeCompare(bValue,locale, { numeric: true, sensitivity: 'variant' });
     return order === 'asc' ? compareResult : -compareResult;
   };
+
+  
 
   const sortTable = (table, columnName, order) => {
     const rows = Array.from(table.tBodies[0].rows);
@@ -21,15 +34,15 @@ document.addEventListener('DOMContentLoaded', () => {
     rows.forEach(row => table.tBodies[0].appendChild(row));
   };
 
-  const handleHeaderClick = (event) => {
-    const header = event.target;
-    const columnName = header.dataset.column;
-    const table = header.closest('table');
-    const order = header.dataset.order === 'asc' ? 'desc' : 'asc';
-    header.dataset.order = order;
+  const sortTableFromButton = (event) => {
+    const button = event.target;
+    const columnName = button.dataset.column;
+    const table = document.querySelector('#detail-table-pc');
+    const order = button.dataset.order === 'asc' ? 'desc' : 'asc';
+    button.dataset.order = order;
     sortTable(table, columnName, order);
   };
 
-  const headers = document.querySelectorAll('#detail-table-pc th');
-  headers.forEach(header => header.addEventListener('click', handleHeaderClick));
+  const buttons = document.querySelectorAll('#button-container button.sort-button');
+  buttons.forEach(button => button.addEventListener('click', sortTableFromButton));
 });
