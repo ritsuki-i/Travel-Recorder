01095ba7e60729331d6a1c29efc410d5734231eb
YukinoSatou <yukino0513@gmail.com>
2024-05-18 17:29:32 +0900

    元に戻す

--- static/js/tableSort.js
+++ static/js/tableSort.js
@@ -1,39 +1,26 @@
-
-document.addEventListener('DOMContentLoaded', () => {
-  const getCellValue = (row, columnName) => {
-    const cell = row.querySelector(`[data-column="${columnName}"]`) || row.children[{
-      'label': 0,
-      'description': 1,
-      'date': 2,
-    }[columnName]];
-
-    if (columnName === 'label') {
-      // columnNameが'label'の場合、ラベルの言語に応じてlocaleを選択
-      const labelValue = cell ? cell.textContent || cell.innerText : '';
-      return labelValue.trim().match(/[\u3040-\u309F\u30A0-\u30FF\u4E00-\u9FAF\u3400-\u4DBF]/) ? 'ja' : 'en';
-    }
-
-
-    return cell ? cell.textContent || cell.innerText : '';
-  };
-
-
-  const compare = (a, b, columnName, order) => {
-    const aValue = getCellValue(a, columnName);
-    const bValue = getCellValue(b, columnName);
-    const locale = columnName === 'label' ? 'ja' : 'en';
-    const compareResult = aValue.localeCompare(bValue,locale, { numeric: true, sensitivity: 'variant' });
-    return order === 'asc' ? compareResult : -compareResult;
-  };
-
+  document.addEventListener('DOMContentLoaded', () => {
+    const getCellValue = (row, columnName) => {
+      const cell = row.querySelector(`[data-column="${columnName}"]`) || row.children[{
+        'label': 0,
+        'description': 1,
+        'date': 2,
+      }[columnName]];
+      return cell ? cell.textContent || cell.innerText : '';
+    };
   
-
-  const sortTable = (table, columnName, order) => {
-    const rows = Array.from(table.tBodies[0].rows);
-    rows.sort((a, b) => compare(a, b, columnName, order));
-    rows.forEach(row => table.tBodies[0].appendChild(row));
-  };
-
+    const compare = (a, b, columnName, order) => {
+      const aValue = getCellValue(a, columnName);
+      const bValue = getCellValue(b, columnName);
+      const compareResult = aValue.localeCompare(bValue, undefined, { numeric: true, sensitivity: 'base' });
+      return order === 'asc' ? compareResult : -compareResult;
+    };
+  
+    const sortTable = (table, columnName, order) => {
+      const rows = Array.from(table.tBodies[0].rows);
+      rows.sort((a, b) => compare(a, b, columnName, order));
+      rows.forEach(row => table.tBodies[0].appendChild(row));
+    };
+  
   const sortTableFromButton = (event) => {
     const button = event.target;
     const columnName = button.dataset.column;
